4c15a7cd3f1285b2d439d90201107281fb963294
Lucas Ribeiro <lucasribeir020@hotmail.com>
2024-03-19 21:09:35 -0300

    Alteração valor do numero secreto

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 99;
 let numeroSecreto = gerarNumeroAleatório();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1','Jogo do número secreto');
-    exibirTextoNaTela('p','Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 99');
 }
 
 exibirMensagemInicial();
